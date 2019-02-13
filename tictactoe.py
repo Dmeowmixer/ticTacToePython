112f7cfbfbc6d37aa0e9580c8d12dc2be9e58a77
Dmeowmixer <ransonikuo@gmail.com>
2019-02-12 15:39:39 -1000

    adds win conditions statically, TOTO check programaticcaly for wins

--- tictactoe.py
+++ tictactoe.py
@@ -26,13 +26,25 @@ player_input()
 
 def place_marker(board, marker, position):
   board[position] = marker
-
 place_marker(game_board, playerOne, 1)
 display_board(game_board)
 
 def win_check(board, mark):
-  print(board[2], board[4], board[6] + mark)
-  if board[2] == mark and board[4] == mark and board[6] == mark:
+  if board[2] == mark and board[5] == mark and board[8] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[1] == mark and board[4] == mark and board[7] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[3] == mark and board[6] == mark and board[9] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[1] == mark and board[5] == mark and board[9] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[3] == mark and board[5] == mark and board[7] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[1] == mark and board[2] == mark and board[3] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[4] == mark and board[5] == mark and board[6] == mark:
+    print(mark + 'Wins THE GAME!')
+  elif board[7] == mark and board[8] == mark and board[9] == mark:
     print(mark + 'Wins THE GAME!')
 
 win_check(game_board, 'O')
