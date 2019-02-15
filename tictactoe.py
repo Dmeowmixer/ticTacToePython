9d0c7e24acdb5f69a003a2e915ab23fa3433aa59
Dmeowmixer <ransonikuo@gmail.com>
2019-02-15 12:37:07 -1000

    adding while loop for game logic, must add in wincheck and player input to update game board

--- tictactoe.py
+++ tictactoe.py
@@ -12,17 +12,28 @@ display_board(game_board)
 playerOne = ''
 playerTwo = ''
 
+playerWon = false
+
+
+
 def player_input():
   print('Player One Choose X or O')
   inputString = ''
   inputString = input().upper()
   print('Player One has chosen '+ inputString)
+
   if inputString == 'X':
     playerTwo = 'O'
+
   else: playerTwo = 'X'
   print('Player Two is '+ playerTwo)
 
 player_input()
+
+while not playerWon:
+  # Play/Continue the game
+
+
 
 def place_marker(board, marker, position):
   board[position] = marker
@@ -32,13 +43,14 @@ display_board(game_board)
 
 def win_check(board, mark):
    if ((board[0] == mark and board[1] == mark and board[2] == mark) or
-        (board[3] == mark and board[4] == mark and  board[5] == mark) or 
-        (board[6] == mark and board[7] == mark and board[8] == mark) or 
+        (board[3] == mark and board[4] == mark and  board[5] == mark) or
+        (board[6] == mark and board[7] == mark and board[8] == mark) or
         (board[5] == mark and board[3] == mark and board[0] == mark) or
         (board[7] == mark and board[4] == mark and board[1] == mark) or
         (board[8] == mark and board[5] == mark and board[2] == mark) or
         (board[0] == mark and board[4] == mark and board[9] == mark) or
         (board[2] == mark and board[4] == mark and board[6] == mark)):
+        playerWon = true
         return 'Player '  + mark + ' has won!'
 
 win_check(game_board, 'O')
